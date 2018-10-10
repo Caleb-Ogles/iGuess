57e191680e66d1b580761a350d7ce0b3b8a3540b
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-10-10 10:55:02 -0500

    added guessing game functionality to the GuessButton action, commented each section of code.

--- iGuess/ViewController.swift
+++ iGuess/ViewController.swift
@@ -11,22 +11,45 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var instructionsLabel: UILabel!
-    @IBOutlet weak var userInputTextField: UITextField!
+    // MARK: UI Outlets
+    @IBOutlet weak var guessButtonOutlet: UIButton!
+    @IBOutlet weak var InstructionsLabel: UILabel!
+    @IBOutlet weak var userGuesstextField: UITextField!
     @IBOutlet weak var feedbackLabel: UILabel!
     @IBOutlet weak var numberOfGuessesLabel: UILabel!
     
+    // MARK UI Actions
     @IBAction func GuessButton(_ sender: Any) {
+        let userInput = userGuesstextField.text!
         
+        guard let guess = Int(userInput) else {
+            feedbackLabel.text = "You didn't give me a number! 🤔"
+            return
+        }
+        
+        guard guess >= 1 && guess <= 100 else {
+            feedbackLabel.text = "Your number wasn't between 0 and 100!! 🤬"
+            return
+        }
+        
+        if guess == randomNumber {
+            feedbackLabel.text = "You Win!!! 🤑"
+        } else if guess < randomNumber {
+            feedbackLabel.text = "Guess higher 👆"
+        } else {
+            feedbackLabel.text = "Guess lower 👇"
+        }
     }
     
+    // MARK: Properties
+    var randomNumber = Int.random(in: 1...100)
+
     
-    
-    
-    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
