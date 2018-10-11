514f8f9cc56c55ee3b55f1d6edca8218ae2e4289
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-10-11 08:40:49 -0500

    Created setUpForRestart and restart functions to handle resetting the game when the user either wins or loses. added more comments where necessary and added a bit of style to the label changes/messages.

--- iGuess/ViewController.swift
+++ iGuess/ViewController.swift
@@ -17,8 +17,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var userGuesstextField: UITextField!
     @IBOutlet weak var feedbackLabel: UILabel!
     @IBOutlet weak var numberOfGuessesLabel: UILabel!
+    @IBOutlet weak var PlayAgain: UIButton!
     
     // MARK UI Actions
+    @IBAction func PlayAgainTapped(_ sender: Any) {
+        restart()
+    }
+    
     @IBAction func GuessButton(_ sender: Any) {
         let userInput = userGuesstextField.text!
         
@@ -27,22 +32,13 @@ class ViewController: UIViewController {
             return
         }
         
-        guard guess >= 1 && guess <= 100 else {
-            feedbackLabel.text = "Your number wasn't between 0 and 100!! 🤬"
-            return
-        }
-        
-        if guess == randomNumber {
-            feedbackLabel.text = "You Win!!! 🤑"
-        } else if guess < randomNumber {
-            feedbackLabel.text = "Guess higher 👆"
-        } else {
-            feedbackLabel.text = "Guess lower 👇"
-        }
+        makeAGuess(guess)
     }
     
     // MARK: Properties
     var randomNumber = Int.random(in: 1...100)
+    var guessesRemaining = 5
+    
 
     
     // MARK: Lifecycle
@@ -51,7 +47,73 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
+    
+    // MARK: Functions
+    
+    func restart(){
+        // new random number
+        randomNumber = Int.random(in: 1...100)
+        // reset guesses remaining
+        guessesRemaining = 5
+        // clear text field
+        userGuesstextField.text = ""
+        // enable text field
+        userGuesstextField.isEnabled = true
+        // hide play again button
+        PlayAgain.isHidden = true
+        // show guess button
+        guessButtonOutlet.isHidden = false
+        // reset feedback label
+        feedbackLabel.text = "Too High/Low"
+        // reset guesses remaining label
+        numberOfGuessesLabel.text = "You have \(guessesRemaining) guesses left"
+    }
+    
+    
+    
+    func setUpUIForRestart() {
+        
+        //disable input
+        userGuesstextField.isEnabled = false
+        
+        //hide guess button
+        guessButtonOutlet.isHidden = true
+        
+        //Unhide play again button
+        PlayAgain.isHidden = false
+    
+    }
 
-
+    
+    func makeAGuess(_ guess: Int) {
+        guard guess >= 1 && guess <= 100 else {
+            feedbackLabel.text = "Your number wasn't between 0 and 100!! 🤬"
+            return
+        }
+        
+        guard guessesRemaining > 0 else {
+            feedbackLabel.text = "You lose!! 🤕"
+            setUpUIForRestart()
+            return
+        }
+        
+        if guess == randomNumber {
+            feedbackLabel.text = "You Win!!! 🤑"
+            setUpUIForRestart()
+            return
+            
+        } else if guess < randomNumber {
+            feedbackLabel.text = "Guess higher 👆"
+            
+        } else {
+            feedbackLabel.text = "Guess lower 👇"
+        }
+        
+        guessesRemaining -= 1
+        numberOfGuessesLabel.text = "You have \(guessesRemaining) guesses left 🤠"
+    }
+    
 }
+
+
 
